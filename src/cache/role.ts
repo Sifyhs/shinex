b03e2ae7ac0884c60d2095b901a27805e78bcad2
Sifyhs <118821886+Sifyhs@users.noreply.github.com>
2024-02-07 07:05:50 +0530

    Update role.ts

--- src/cache/role.ts
+++ src/cache/role.ts
@@ -1,6 +1,6 @@
 export const ShinexOwners = new Set<string>([
   '1087029915393736827',
-  '1138356763419213916'
+  '1165498072709726229
 ])
 export const ShinexAdmins = new Set<string>()
 export const ShinexSeniorMods = new Set<string>()
